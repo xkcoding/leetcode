1a8a3e61a942698a550f69668b3ac298605d725b
Yangkai.Shen <237497819@qq.com>
2020-07-13 09:21:12 +0800

    :sparkles: No.1

--- src/main/java/com/xkcoding/leetcode/code1/Solution1.java
+++ src/main/java/com/xkcoding/leetcode/code1/Solution1.java
@@ -11,6 +11,10 @@ package com.xkcoding.leetcode.code1;
  * 链接：https://leetcode-cn.com/problems/two-sum
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  * </p>
+ * {@code 给定 nums = [2, 7, 11, 15], target = 9
+ *
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]}
  *
  * @author yangkai.shen
  * @date Created in 2020-07-02 13:25
