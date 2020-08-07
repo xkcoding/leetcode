a5ca42b896f0d74dc8d7c4e9289348ba75691a1d
Yangkai.Shen <237497819@qq.com>
2020-08-07 14:50:40 +0800

    :sparkles: No.100

---
+++ src/main/java/com/xkcoding/leetcode/code100/TreeNode.java
@@ -0,0 +1,20 @@
+package com.xkcoding.leetcode.code100;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
